89d919a71ea369be72460b2c75e6ec7932773d26
KUNAL <116474183+ks6024108@users.noreply.github.com>
2024-08-03 20:43:47 +0530

    Add files via upload

---
+++ some Basics/RemoveCharacterFromString.cpp
@@ -0,0 +1,27 @@
+//  remove all characters from a string except alphabets
+#include <cctype>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string remove_non_alphabets(string str)
+{
+    string result = "";
+    for (char c : str) {
+        if (isalpha(c)) {
+            result += c;
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    string str = "unnu@kunnu2005";
+
+    cout << "Alphabets only: " << remove_non_alphabets(str)
+         << endl;
+
+    return 0;
+}
